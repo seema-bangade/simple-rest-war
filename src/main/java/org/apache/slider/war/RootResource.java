783e08262dd19fc87ba4599aa843abbcee7d96b0
seema-bangade <seema.l.bangade@gmail.com>
2026-07-07 08:35:20 +0530

    Disable getIt method in RootResource
    Comment out the getIt method and its annotations.

--- src/main/java/org/apache/slider/war/RootResource.java
+++ src/main/java/org/apache/slider/war/RootResource.java
@@ -17,9 +17,9 @@ public class RootResource {
      *
      * @return String that will be returned as a text/plain response.
      */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Success";
-    }
+    // @GET
+    // @Produces(MediaType.TEXT_PLAIN)
+    // public String getIt() {
+    //     return "Success";
+    // }
 }
